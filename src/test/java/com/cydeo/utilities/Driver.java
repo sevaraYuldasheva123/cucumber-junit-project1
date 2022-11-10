eca821de0fd9647742261355a1b4581c325926bb
Sevara Yuldasheva <sevara878787@mail.ru>
2022-11-10 15:39:22 -0500

    some changes IN DRIVER CLASS

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -4,7 +4,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
@@ -28,14 +31,54 @@ public class Driver {
     public static WebDriver getDriver(){
 
         if(driverPool.get() == null){  // if driver/browser was never opened
+            String browserType = "";
 
-        String browserType = ConfigurationReader.getProperty("browser");
+            if (System.getProperty("BROWSER") == null){
+                browserType = ConfigurationReader.getProperty("browser");
+            }else {
+                browserType = System.getProperty("BROWSER");
+            }
+
+        //String browserType = ConfigurationReader.getProperty("browser");
 
         /*
         Depending on the browserType our switch statement will determine
         to open specific type of browser/driver
          */
         switch(browserType){
+            case "remote-chrome":
+                try {
+                    // assign your grid server address
+                    String gridAddress = "54.197.137.191";
+                    URL url = new URL("http://"+ gridAddress + ":4444/wd/hub");
+                    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                    desiredCapabilities.setBrowserName("chrome");
+                    driverPool.set(new RemoteWebDriver(url, desiredCapabilities));
+                    //driverPool.set(new RemoteWebDriver(new URL("http://0.0.0.0:4444/wd/hub"),desiredCapabilities));
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "remote-firefox":
+                try {
+                    // assign your grid server address
+                    String gridAddress = "54.197.137.191";
+                    URL url = new URL("http://"+ gridAddress + ":4444/wd/hub");
+                    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                    desiredCapabilities.setBrowserName("firefox");
+                    driverPool.set(new RemoteWebDriver(url, desiredCapabilities));
+                    //driverPool.set(new RemoteWebDriver(new URL("http://0.0.0.0:4444/wd/hub"),desiredCapabilities));
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+
+
+
+
+
             case "chrome":
                 WebDriverManager.chromedriver().setup();
                 driverPool.set(new ChromeDriver());
